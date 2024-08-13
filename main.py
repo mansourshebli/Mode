0efbe6b57f8b1409c87d907a3b71324cf092003e
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2024-08-13 20:20:22 +0400

    Starter config

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import tkinter as tk
+from tkinter import *
+import csv
+
+# App config
+app = Tk()
+app.geometry('450x450')
+app.title('Mode')
+
+# Widgets
+mainFrame = Frame(app)
+mainFrame.pack()
+
+welcomeLabel = Label(mainFrame, text='Welcome to Mode!', font=('Arial', 35))
+welcomeLabel.pack()
+
+controlFrame = Frame(mainFrame)
+controlFrame.pack()
+
+
+app.mainloop()
