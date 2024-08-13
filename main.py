8bdba09b923acc250513deb99477ad90752743a2
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2024-08-13 20:54:20 +0400

    Added light mode image widgets

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from tkinter import *
-import csv
+from tkinter import filedialog
+import os
+
+# Functions
+def lightModeImage():
+    filetypes = [("Image files", "*.png *.jpg *.jpeg")]
+
+    LMimagePath = filedialog.askopenfilename(title='Select your light mode image', filetypes=filetypes)
+    if LMimagePath: 
+        LMimagePath = os.path.relpath(LMimagePath)
+        print(f"Selected image path: {LMimagePath}")
+    else:
+        print("No file selected.")
 
 # App config
 app = Tk()
-app.geometry('450x450')
+app.geometry('750x650')
 app.title('Mode')
 
 # Widgets
@@ -13,8 +25,11 @@ mainFrame.pack()
 welcomeLabel = Label(mainFrame, text='Welcome to Mode!', font=('Arial', 35))
 welcomeLabel.pack()
 
-controlFrame = Frame(mainFrame)
-controlFrame.pack()
+controlFrame = Frame(app)
+controlFrame.pack(padx=20, pady=70, fill=X)
 
+LMimageButton = Button(controlFrame, text='Upload Light Mode image', command=lightModeImage, bg='lightgray', font=('Arial', 15))
+LMimageButton.pack(side=LEFT)
 
 app.mainloop()
+
