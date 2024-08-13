b17e2a7b99f0691f7464501de26c6bd6bc660266
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2024-08-13 21:10:58 +0400

    Image preview for light mode image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image, ImageTk
 import os
 
 # Functions
@@ -10,8 +11,17 @@ def lightModeImage():
     if LMimagePath: 
         LMimagePath = os.path.relpath(LMimagePath)
         print(f"Selected image path: {LMimagePath}")
+
+        image = Image.open(LMimagePath)
+        image.thumbnail((150, 150))
+        LMimage = ImageTk.PhotoImage(image)
+
+        LMimagePreview.config(image=LMimage)
+        LMimagePreview.image = LMimage
+        
     else:
         print("No file selected.")
+        LMimagePreview.config(image='')
 
 # App config
 app = Tk()
@@ -28,8 +38,13 @@ welcomeLabel.pack()
 controlFrame = Frame(app)
 controlFrame.pack(padx=20, pady=70, fill=X)
 
+# LIGHT MODE WIDGETS
 LMimageButton = Button(controlFrame, text='Upload Light Mode image', command=lightModeImage, bg='lightgray', font=('Arial', 15))
 LMimageButton.pack(side=LEFT)
+
+LMimagePreview = Label(controlFrame, bg='white', width=150, heigh=150, borderwidth=2, relief='solid')
+LMimagePreview.pack(padx=20, pady=20)
+
 
 app.mainloop()
 
